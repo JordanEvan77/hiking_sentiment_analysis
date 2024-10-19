0313f401d547180f9d878b2c158481ea0ecc4869
JordanEvan77 <jgropper@seattleu.edu>
2024-10-19 13:11:50 -0700

    javascript, captured hike names

--- a0_explore_html.py
+++ a0_explore_html.py
@@ -1,14 +1,7 @@
 import pandas as pd
 import numpy as np
-import requests
-from bs4 import BeautifulSoup
-
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://www.wta.org/go-outside/trip-reports'
-url2 = 'https://www.wta.org/go-hiking/trip-reports/trip_report-2024-10-19.114407414301'
-response = requests.get(url)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # This is a main trip report page, and after clicking on each report (trail name and date) the
 # details are underneath
@@ -29,26 +22,40 @@ response = requests.get(url)
         # Highest Point
         # Maybe grab other category characteristics? later?
 
-page = requests.get(url)
-print(page.text)
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="trip-reports")
-print(results.prettify())
-#----
-hike_titles = soup.find_all('h3', class_='listitem-title')
+driver = webdriver.Chrome()
 
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    trip_reports = soup.find_all('h2', class_='trip-report-title')
+url = 'https://www.wta.org/go-outside/trip-reports'
+driver.get(url)
+driver.implicitly_wait(10)
+#-----------
+page_source = driver.page_source
+from bs4 import BeautifulSoup
 
-    for report in trip_reports:
-        print('report', report.text)
-else:
-    print('Failed to retrieve the webpage')
+# Parse the HTML content with BeautifulSoup
+soup = BeautifulSoup(page_source, 'html.parser')
+
+# Extract hike titles and dates
+hike_entries = soup.find_all('h3', class_='listitem-title')
+
+hike_list = [entry.text.strip() for entry in hike_entries]
+for hike in hike_list:
+    print(hike)
+
+#------
+all_elements = driver.find_elements(By.CSS_SELECTOR, '*')
+
+
+for element in all_elements[:1]:
+    print('tag', element.tag_name, 'CLass', element.get_attribute('class'), 'TEXT', element.text)
 
 
 
+hike_titles = driver.find_elements(By.CSS_SELECTOR, 'div.listitem-title a')
 
 
+for title in hike_titles:
+    print(title.text)
+
+driver.quit()
