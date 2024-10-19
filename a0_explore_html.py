17cd34b6dfbdcab22871b6feff8305ad50a9fb0c
JordanEvan77 <jgropper@seattleu.edu>
2024-10-19 15:11:13 -0700

    inner loop complete, now change the page to next 100

--- a0_explore_html.py
+++ a0_explore_html.py
@@ -50,37 +50,15 @@ raw_df = pd.DataFrame({'Hike Name': [],
                        'Snow': []
                        # and the actual written report
                        })
-for entry in hike_entries:
-    print('Count of hikes', len(hike_entries))
-    link = entry.find('a')
-    if link:
-        print(f'Navigating to: {link.text.strip()}')
-        href = link.get('href')
-        driver.get(href)
 
-        # load time
-        time.sleep(5)
-
-        # get needed parts
-        page_source = driver.page_source
-        soup_middle = BeautifulSoup(page_source, 'html.parser')
-
-        # Extract the specific details
-        details = soup_middle.find_all('div', class_='trip-condition')
-        detail_dict = {detail.find('h4').text.strip(): detail.find('span').text.strip()
-            for detail in details}
-        hike_title = soup_middle.find('span', id='breadcrumbs-current').text.strip() if \
-            soup_middle.find('span', id='breadcrumbs-current') else 'N/A'
-        trail_report_by = soup_middle.find('span', class_='wta-icon-headline__text').text.strip() \
-            if soup_middle.find('span', class_='wta-icon-headline__text') else 'N/A'
-        report_text = soup_middle.find('div', id='tripreport-body-text').get_text(
-            strip=True) if soup_middle.find('div', id='tripreport-body-text') else 'N/A'
-
-        # ONE LAYER FURTHER IN!
-        trails_hiked_link = soup_middle.find('div', class_="related-hike-links")
-        link = trails_hiked_link.find('a')
+while True:
+    i = 0
+    for entry in hike_entries:
+        i +=1
+        print('Count of hikes', i, '/', len(hike_entries))
+        link = entry.find('a')
         if link:
-            print(f'Navigating to inner details: {link.text.strip()}')
+            print(f'Navigating to: {link.text.strip()}')
             href = link.get('href')
             driver.get(href)
 
@@ -89,56 +67,90 @@ for entry in hike_entries:
 
             # get needed parts
             page_source = driver.page_source
-            soup_inner = BeautifulSoup(page_source, 'html.parser')
+            soup_middle = BeautifulSoup(page_source, 'html.parser')
+
+            # Extract the specific details
+            details = soup_middle.find_all('div', class_='trip-condition')
+            detail_dict = {detail.find('h4').text.strip(): detail.find('span').text.strip()
+                for detail in details}
+            hike_title = soup_middle.find('span', id='breadcrumbs-current').text.strip() if \
+                soup_middle.find('span', id='breadcrumbs-current') else 'N/A'
+            trail_report_by = soup_middle.find('span', class_='wta-icon-headline__text').text.strip() \
+                if soup_middle.find('span', class_='wta-icon-headline__text') else 'N/A'
+            report_text = soup_middle.find('div', id='tripreport-body-text').get_text(
+                strip=True) if soup_middle.find('div', id='tripreport-body-text') else 'N/A'
+
+            # ONE LAYER FURTHER IN!
+            trails_hiked_link = soup_middle.find('div', class_="related-hike-links")
+            link = trails_hiked_link.find('a')
+            if link:
+                #print(f'Navigating to inner details: {link.text.strip()}')
+                href = link.get('href')
+                driver.get(href)
+
+                # load time
+                time.sleep(5)
+
+                # get needed parts
+                page_source = driver.page_source
+                soup_inner = BeautifulSoup(page_source, 'html.parser')
 
 
-            stats = soup_inner.find_all('div', class_='hike-stats__stat')
-            for stat in stats:
-                detail_dict[stat.find('dt').text.strip()] = stat.find('dd').text.strip()
+                stats = soup_inner.find_all('div', class_='hike-stats__stat')
+                for stat in stats:
+                    detail_dict[stat.find('dt').text.strip()] = stat.find('dd').text.strip()
 
-            region_info = soup_inner.find('div', class_='region')
-            if region_info:
-                trailhead_region = region_info.find('span',  class_='region').text.strip()
+                region_info = soup_inner.find('div', class_='region')
+                if region_info:
+                    trailhead_region = region_info.find('span',  class_='region').text.strip()
 
-            rating_div = soup_inner.find('div', class_='current-rating')
-            rating = rating_div.text.strip()
-            # now for the icon list:
-            icons_list = []
-            icons_section = soup_inner.find('ul', class_='wta-icon-list')
-            if icons_section:
-                icons = icons_section.find_all('li')
-                for icon in icons:
-                    icon_label = icon.find('span', class_='wta-icon__label').text.strip()
-                    icons_list.append(icon_label)
+                rating_div = soup_inner.find('div', class_='current-rating')
+                rating = rating_div.text.strip()
+                # now for the icon list:
+                icons_list = []
+                icons_section = soup_inner.find('ul', class_='wta-icon-list')
+                if icons_section:
+                    icons = icons_section.find_all('li')
+                    for icon in icons:
+                        icon_label = icon.find('span', class_='wta-icon__label').text.strip()
+                        icons_list.append(icon_label)
 
-        #Now a temp df
-        detail_df = pd.DataFrame({
-            'Hike Name': [hike_title],
-            'Trail Report By': [trail_report_by],
-            'Type of Hike': [detail_dict['Type of Hike']],
-            'Trail Conditions': [detail_dict['Trail Conditions']],
-            'Road': [detail_dict['Road']],
-            'Bugs': [detail_dict['Bugs']],
-            'Snow': [detail_dict['Snow']],
-            'Report Text': [report_text],
-            'Region': [trailhead_region],
-            'Elevation': [detail_dict['Elevation Gain']],
-            'Highest Point': [detail_dict['Highest Point']],
-            'Difficulty': [detail_dict['Calculated Difficulty\n                            '
-                                       '\n\nAbout Calculated Difficulty']],
-            'Rating': [rating],
-            'Key Features': [icons_list]
+            #Now a temp df
+            detail_df = pd.DataFrame({
+                'Hike Name': [hike_title],
+                'Trail Report By': [trail_report_by],
+                'Type of Hike': [detail_dict['Type of Hike']],
+                'Trail Conditions': [detail_dict['Trail Conditions']],
+                'Road': [detail_dict['Road']],
+                'Bugs': [detail_dict['Bugs']],
+                'Snow': [detail_dict['Snow']],
+                'Report Text': [report_text],
+                'Region': [trailhead_region],
+                'Elevation': [detail_dict['Elevation Gain']],
+                'Highest Point': [detail_dict['Highest Point']],
+                'Difficulty': [detail_dict['Calculated Difficulty\n                            '
+                                           '\n\nAbout Calculated Difficulty']],
+                'Rating': [rating],
+                'Key Features': [icons_list]
 
-        })
-        raw_df = pd.concat([raw_df, detail_df], axis=0)
-        # now in one final layer
-        
-        #back out
-        driver.back()
-        driver.implicitly_wait(10)
+            })
+            raw_df = pd.concat([raw_df, detail_df], axis=0)
+            # now in one final layer
 
+            #back out
+            driver.back()
+            driver.implicitly_wait(10)
+    # Check if there is a "Next 100 items" button
+    next_button = soup_outer.find('a', text='Next 100 items')
+    if next_button:
+        next_button_link = next_button['href']
+        driver.get(next_button_link)
+        time.sleep(5)
+    else:
+        print('clicked through all')
+        break
 
-driver.quit()
+    driver.quit()
 
 
 raw_df.to_csv(data_dir + 'hiking_reports.csv', index=False)
