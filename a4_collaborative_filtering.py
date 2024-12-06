adfa434b379c785f7cffaed1420498b73207a12a
JordanEvan77 <jgropper@seattleu.edu>
2024-12-05 20:21:56 -0800

    y is empty now?

--- a4_collaborative_filtering.py
+++ a4_collaborative_filtering.py
@@ -8,22 +8,23 @@ from Scratch.loggers import data_dir, data_out
 
 
 def split_data_and_train(df_model):
-    hike_attributes = [i for i in df_model.columns if i not in ['sentiment']]
+    hike_attributes = [i for i in df_model.columns if i not in ['sentiment', 'reviewer_id',
+                                                                'hike_id']]
     X = df_model[hike_attributes]
     y = df_model[['sentiment']]
     reviewer_ids = df_model[['reviewer_id']]
-
-    X_train, X_test, y_train, y_test, train_ids, test_ids = train_test_split(X, y, reviewer_ids,
-                               test_size=0.2, random_state=42)
+    hike_ids = df_model[['hike_id']]
+    X_train, X_test, y_train, y_test, train_ids, test_ids, train_hike_ids, test_hike_ids = train_test_split(
+        X, y, reviewer_ids, hike_ids, test_size=0.2, random_state=42, stratify=y)
     # so that I can keep track of everyone
 
     #fix index, inplace is outdated
-    X_train = X_train.reset_index(drop=True)
-    X_test = X_test.reset_index(drop=True)
-    y_train = y_train.reset_index(drop=True)
-    y_test = y_test.reset_index(drop=True)
-    train_ids = train_ids.reset_index(drop=True)
-    test_ids = test_ids.reset_index(drop=True)
+    # X_train = X_train.reset_index(drop=True)
+    # X_test = X_test.reset_index(drop=True)
+    # y_train = y_train.reset_index(drop=True)
+    # y_test = y_test.reset_index(drop=True)
+    # train_ids = train_ids.reset_index(drop=True)
+    # test_ids = test_ids.reset_index(drop=True)
 
 
 
@@ -34,7 +35,7 @@ def split_data_and_train(df_model):
     distances, indices = model_knn.kneighbors(X_test)
     #preds
     predicted_sentiments = []
-    for idx in indices: predicted_sentiments.append(y_train.iloc[idx].mode().iloc[0])
+    for idx in indices: predicted_sentiments.append(y_train.loc[idx].mode().iloc[0])
 
     # Now see if the model is accurate
     accuracy = accuracy_score(y_test, predicted_sentiments)
@@ -45,22 +46,23 @@ def split_data_and_train(df_model):
 
 ## I want the ids in the test set to then get their highest possible recommendation from their
 # nearest neighbors:
-def get_recommendations(test_ids, indices, X_train, y_train):
+def get_recommendations(test_ids, indices, X_train, y_train, test_hike_ids):
     recommendations = []
-    for i, reviewer_id in enumerate(test_ids['reviewer_id'].unique()):
+    for reviewer_id in test_ids['reviewer_id'].unique():
         idx = test_ids[test_ids['reviewer_id'] == reviewer_id].index
         if not idx.empty:
             neighbor_idx = indices[idx[0]]
             similar_hikes = X_train.iloc[neighbor_idx]
-            similar_hikes.loc['sentiment'] = y_train.iloc[neighbor_idx].values
+            similar_hikes['sentiment'] = y_train.iloc[neighbor_idx].values
+            similar_hikes['hike_id'] = test_hike_ids.iloc[neighbor_idx].values
             highest_sentiment_hike = similar_hikes.loc[similar_hikes['sentiment'].idxmax()]
             recommendations.append((reviewer_id, highest_sentiment_hike))
 
-    # Check recs
+    # Check recommendations
     print('Recommendations for each unique reviewer ID in the test set:')
     for reviewer_id, hike in recommendations:
         print(f'Reviewer ID: {reviewer_id}')
-        print(f'Was reccomended hike: {hike}')
+        print(f'Recommended hike: {hike}')
 
 
 
@@ -74,7 +76,7 @@ if __name__ == '__main__':
     print('Start Filtering')
 
     df_model = pd.read_csv(data_out + 'model_data1_no_pca.csv')
-    df_model.set_index('hike_id', inplace=True)
+    #df_model.set_index('hike_id', inplace=True)
     #df_final_pca.to_csv(data_out + 'model_data1_pca.csv', index=False)
 
     X_train, X_test, y_train, y_test, train_ids, test_ids, model_knn, indices = \
